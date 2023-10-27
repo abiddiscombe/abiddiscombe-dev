933dbd0d65b90d6d7c8ed0c04d449864b8235ab5
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-10-27 18:00:55 +0100

    fix: WCAG text contrast footer violation

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className='px-8 py-4 bg-zinc-200 dark:bg-zinc-900 text-zinc-500'>
+    <footer className='px-8 py-4 bg-zinc-100 dark:bg-zinc-900 text-zinc-600 dark:text-zinc-400'>
       <div className='mx-auto max-w-2xl'>
         <small>&copy; Archie Biddiscombe 2023</small>
       </div>
