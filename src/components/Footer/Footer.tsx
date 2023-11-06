0051e0cfd59daffe35fbafc442bdbe5e79c7f60e
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-11-06 20:32:32 +0000

    fix: disparity between header and footer bg values

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import { ExternalLink } from '../ExternalLink';
 
 export function Footer() {
   return (
-    <footer className='px-8 py-4 bg-zinc-100 dark:bg-zinc-900 text-zinc-600 dark:text-zinc-400'>
+    <footer className='px-8 py-4 bg-zinc-100 dark:bg-[#222] text-zinc-600 dark:text-zinc-400'>
       <div className='mx-auto max-w-2xl flex flex-wrap gap-2 items-center justify-between'>
         <small>&copy; Archie Biddiscombe 2023</small>
         <small>
