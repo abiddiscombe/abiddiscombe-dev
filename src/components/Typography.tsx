5d0b2447ece288604c7bb00b6331c4b89c02a7bb
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-11-17 14:20:56 +0000

    fix: project link icon (wip)

--- src/components/Typography.tsx
+++ src/components/Typography.tsx
@@ -17,12 +17,9 @@ export function Typography(p: TypographyProps) {
 
     case 'h3':
       return (
-        <h3
-          className={
-            "tracking-wide leading-relaxed font-semibold after:text-xs after:text-fontLightest-light after:pl-1 dark:after:text-fontLightest-dark hover:after:text-fontLighter-light hover:dark:after:text-fontLighter-dark after:content-['_↗']"
-          }
-        >
-          {p.children}
+        <h3 className={'flex items-center tracking-wide leading-relaxed font-semibold'}>
+          <span className='mr-1'>{p.children}</span>
+          <ProjectLinkIcon />
         </h3>
       );
 
@@ -36,4 +33,24 @@ export function Typography(p: TypographyProps) {
         </p>
       );
   }
+}
+
+function ProjectLinkIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke='currentColor'
+      strokeWidth='2'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      className='h-4 text-fontLighter-light dark:text-fontLighter-dark feather feather-arrow-up-right'
+    >
+      <line x1='7' y1='17' x2='17' y2='7'></line>
+      <polyline points='7 7 17 7 17 17'></polyline>
+    </svg>
+  );
 }
