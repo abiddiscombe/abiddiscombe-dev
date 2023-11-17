1823eb2477f7103cc9a5c850b13e5e5139891157
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-11-17 14:21:28 +0000

    fix: accessibility in social link

--- src/components/SocialLink.tsx
+++ src/components/SocialLink.tsx
@@ -14,28 +14,48 @@ export function SocialLink(p: SocialLinkProps) {
   switch (p.type) {
     case 'email':
       return (
-        <a href={email} target='_blank' className={genericClasses}>
+        <a
+          href={email}
+          target='_blank'
+          aria-label='Email (Opens in Your Email Client)'
+          className={genericClasses}
+        >
           <EmailIcon />
         </a>
       );
 
     case 'github':
       return (
-        <a href={github} target='_blank' className={genericClasses}>
+        <a
+          href={github}
+          target='_blank'
+          aria-label='GitHub (Opens in a New Tab)'
+          className={genericClasses}
+        >
           <GithubIcon />
         </a>
       );
 
     case 'linkedin':
       return (
-        <a href={linkedin} target='_blank' className={genericClasses}>
+        <a
+          href={linkedin}
+          target='_blank'
+          aria-label='LinkedIn (Opens in a New Tab)'
+          className={genericClasses}
+        >
           <LinkedinIcon />
         </a>
       );
 
     case 'literature':
       return (
-        <a href={literature} target='_blank' className={genericClasses}>
+        <a
+          href={literature}
+          target='_blank'
+          aria-label='Literature on GitHub (Opens in a New Tab)'
+          className={genericClasses}
+        >
           <LiteratureIcon />
         </a>
       );
@@ -59,6 +79,7 @@ function EmailIcon() {
       strokeLinejoin='round'
       className='feather feather-at-sign h-6 md:h-5'
     >
+      <title>Email Icon</title>
       <circle cx='12' cy='12' r='4'></circle>
       <path d='M16 8v5a3 3 0 0 0 6 0v-1a10 10 0 1 0-3.92 7.94'></path>
     </svg>
@@ -79,6 +100,7 @@ function GithubIcon() {
       strokeLinejoin='round'
       className='feather feather-github h-6 md:h-5'
     >
+      <title>GitHub Icon</title>
       <path d='M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22'></path>
     </svg>
   );
@@ -98,6 +120,7 @@ function LinkedinIcon() {
       strokeLinejoin='round'
       className='feather feather-linkedin h-6 md:h-5'
     >
+      <title>LinkedIn Icon</title>
       <path d='M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z'></path>
       <rect x='2' y='9' width='4' height='12'></rect>
       <circle cx='4' cy='4' r='2'></circle>
@@ -119,6 +142,7 @@ function LiteratureIcon() {
       strokeLinejoin='round'
       className='feather feather-book h-6 md:h-5'
     >
+      <title>Book Icon</title>
       <path d='M4 19.5A2.5 2.5 0 0 1 6.5 17H20'></path>
       <path d='M6.5 2H20v20H6.5A2.5 2.5 0 0 1 4 19.5v-15A2.5 2.5 0 0 1 6.5 2z'></path>
     </svg>
