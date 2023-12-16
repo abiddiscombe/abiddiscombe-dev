fb8dc0f3eac99aaae491cf016db803a247ad91e9
abiddiscombe <76795128+abiddiscombe@users.noreply.github.com>
2023-12-16 18:05:03 +0000

    feat: add framer-motion animations

--- CHANGES.md
+++ CHANGES.md
@@ -3,6 +3,7 @@
 ## 2.7.0 - December 2023
 
 1. feat: remove contour background
+2. feat: add framer-motion animations
 
 ## 2.6.0 - December 2023
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { motion } from 'framer-motion';
 import { projects } from '@/data/projects';
 import { Typography } from '@/components/Typography';
 import { ExternalLink } from '@/components/ExternalLink';
@@ -5,37 +7,60 @@ import { ExternalLink } from '@/components/ExternalLink';
 export default function Home() {
   return (
     <>
-      <Typography variant='body'>
-        Hey! I&apos;m an Associate Software Engineer, working for{' '}
-        <ExternalLink href='https://www.os.uk' message='Ordnance Survey' />.
-      </Typography>
+      <motion.div
+        initial={{ opacity: 0, y: 14 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0 }}
+      >
+        <Typography variant='body'>
+          Hey! I&apos;m an Associate Software Engineer, working for{' '}
+          <ExternalLink href='https://www.os.uk' message='Ordnance Survey' />.
+        </Typography>
+      </motion.div>
 
-      <Typography variant='body'>
-        I&apos;m interested in designing <ExternalLink href='https://react.dev' message='React' />
-        -based web applications, exploring new geospatial technologies, and building APIs or other
-        backend things. I&apos;m often deploying to{' '}
-        <ExternalLink href='https://azure.com' message='Azure' />,{' '}
-        <ExternalLink href='https://cloudflare.com' message='Cloudflare' />, or with{' '}
-        <ExternalLink href='https://docker.com' message='Docker' /> on my home server.
-      </Typography>
+      <motion.div
+        initial={{ opacity: 0, y: 14 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.2 }}
+      >
+        <Typography variant='body'>
+          I&apos;m interested in designing <ExternalLink href='https://react.dev' message='React' />
+          -based web applications, exploring new geospatial technologies, and building APIs or other
+          backend things. I&apos;m often deploying to{' '}
+          <ExternalLink href='https://azure.com' message='Azure' />,{' '}
+          <ExternalLink href='https://cloudflare.com' message='Cloudflare' />, or with{' '}
+          <ExternalLink href='https://docker.com' message='Docker' /> on my home server.
+        </Typography>
+      </motion.div>
 
-      <Typography variant='body'>
-        Away from the desk, I love cooking, Spotify, and exploring new places. I have a BSc in
-        Geography from{' '}
-        <ExternalLink
-          href='https://www.durham.ac.uk/departments/academic/geography/'
-          message='Durham University'
-        />{' '}
-        where I specialised in GIS and Glaciology.
-      </Typography>
+      <motion.div
+        initial={{ opacity: 0, y: 14 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.4 }}
+      >
+        <Typography variant='body'>
+          Away from the desk, I love cooking, Spotify, and exploring new places. I have a BSc in
+          Geography from{' '}
+          <ExternalLink
+            href='https://www.durham.ac.uk/departments/academic/geography/'
+            message='Durham University'
+          />{' '}
+          where I specialised in GIS and Glaciology.
+        </Typography>
+      </motion.div>
 
-      <Typography variant='h2'>Recent Projects</Typography>
-
-      <div className='grid gap-4 grid-cols-1 sm:grid-cols-2'>
-        {projects.map((project) => (
-          <ProjectCard key={project.href} {...project} />
-        ))}
-      </div>
+      <motion.div
+        initial={{ opacity: 0, y: 14 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.6 }}
+      >
+        <Typography variant='h2'>Recent Projects</Typography>
+        <div className='grid gap-4 grid-cols-1 sm:grid-cols-2'>
+          {projects.map((project) => (
+            <ProjectCard key={project.href} {...project} />
+          ))}
+        </div>
+      </motion.div>
     </>
   );
 }
